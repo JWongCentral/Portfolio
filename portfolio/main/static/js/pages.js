38ef81ef755b4148edcd27c098add7aefbff20c5
JWong <JackBWong1998@gmail.com>
2023-06-27 14:16:09 -0400

    adding in images

--- portfolio/main/static/js/pages.js
+++ portfolio/main/static/js/pages.js
@@ -3,11 +3,47 @@
 //if it is in view, then we can finish off the page/scrolling transition in css
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry);
+        console.log(entry.target.classList);
         if(entry.isIntersecting){
             entry.target.classList.add('show');
+            entry.target.classList.remove('hide');
+            if(entry.target.classList.contains("one")){
+                home = document.querySelector(".home")
+                home.classList.add("active-page")
+                console.log(home)
+            }
+            else if(entry.target.classList.contains("two")){
+                expertise = document.querySelector(".expertise")
+                expertise.classList.add("active-page")
+                
+            }
+            else if(entry.target.classList.contains("three")){
+                projects = document.querySelector(".projects")
+                projects.classList.add("active-page")
+            }
+            else if(entry.target.classList.contains("four")){
+                contact = document.querySelector(".contact")
+                contact.classList.add("active-page")
+            }
         } else {
             entry.target.classList.remove('show');
+            if(entry.target.classList.contains("one")){
+                home = document.querySelector(".home")
+                home.classList.remove("active-page")
+            }
+            else if(entry.target.classList.contains("two")){
+                expertise = document.querySelector(".expertise")
+                expertise.classList.remove("active-page")
+                
+            }
+            else if(entry.target.classList.contains("three")){
+                projects = document.querySelector(".projects")
+                projects.classList.remove("active-page")
+            }
+            else if(entry.target.classList.contains("four")){
+                contact = document.querySelector(".contact")
+                contact.classList.remove("active-page")
+            }
         }
     });
 });
@@ -17,4 +53,3 @@ const hiddenElements = document.querySelectorAll(".page");
 hiddenElements.forEach((el) => {
     observer.observe(el)
 })
-console.log(hiddenElements);
