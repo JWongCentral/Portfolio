034f5e9fdf2e03832f2f103a216e5a69e6c3a751
JWong <JackBWong1998@gmail.com>
2023-08-30 18:10:36 -0400

    Adding in mongodb script and finishing API and some basic html for financial app

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    
+    'financial_app'
 ]
 
 MIDDLEWARE = [
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -19,5 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("main.urls"))
+    path('financial_app/',include('financial_app.urls')),
+    path('',include("main.urls")),
+    
 ]
